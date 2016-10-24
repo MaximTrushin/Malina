ee66403f24840fe7407fe9e49b4a9600a861bf8f
Maxim Trushin <trushin@gmail.com>
2016-10-24 17:05:16 -0400

    Changed SourceLocation to be XML serializable.

--- Malina.DOM/SourceLocation.cs
+++ Malina.DOM/SourceLocation.cs
@@ -6,26 +6,28 @@ namespace Malina.DOM
     public class SourceLocation : IComparable<SourceLocation>, IEquatable<SourceLocation>
     {
         // Fields
-        private readonly int _column;
-        private readonly int _line;
-        private readonly int _index;
+        public int Column;
+        public int Line;
+        public int Index;
 
         // Methods
         public SourceLocation(int line, int column, int index)
         {
-            _line = line;
-            _column = column;
-            _index = index;
+            Line = line;
+            Column = column;
+            Index = index;
         }
+
+        public SourceLocation() { }
 
         public int CompareTo(SourceLocation other)
         {
-            int num = _line.CompareTo(other._line);
+            int num = Line.CompareTo(other.Line);
             if (num != 0)
             {
                 return num;
             }
-            return _column.CompareTo(other._column);
+            return Column.CompareTo(other.Column);
         }
 
         public bool Equals(SourceLocation other)
@@ -35,41 +37,20 @@ namespace Malina.DOM
 
         public override string ToString()
         {
-            return string.Format("({0},{1},{2})", _line, _column, _index);
+            return string.Format("({0},{1},{2})", Line, Column, Index);
         }
 
         // Properties
-        public int Column
-        {
-            get
-            {
-                return _column;
-            }
-        }
 
         public virtual bool IsValid
         {
             get
             {
-                return ((_line > 0) && (_column > 0));
+                return ((Line > 0) && (Column > 0));
             }
         }
 
-        public int Line
-        {
-            get
-            {
-                return _line;
-            }
-        }
 
-        public int Index
-        {
-            get
-            {
-                return _index;
-            }
-        }
     }
 
 

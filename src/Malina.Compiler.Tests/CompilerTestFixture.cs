f23e864797b13039c48ce2d33a0aebaedd0da9c0
Maxim Trushin <trushin@gmail.com>
2016-11-04 10:37:34 -0400

    Initial commit for Malina.Compiler.Tests.

---
+++ src/Malina.Compiler.Tests/CompilerTestFixture.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using static Malina.Compiler.Tests.TestUtils;
+
+namespace Malina.Compiler.Tests
+{
+    [TestFixture]
+    public class CompilerTestFixture
+    {
+        [Test]
+        public void ModuleWithDocumentAndAlias()
+        {
+            PerformTest();
+        }
+    }
+}
---
+++ src/Malina.Compiler.Tests/TestUtils.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Malina.Compiler;
+using Malina.Compiler.Pipelines;
+
+namespace Malina.Compiler.Tests
+{
+    public class TestUtils
+    {
+        public static void PerformTest()
+        {
+            var compilerParameters = CreateCompilerParameters();
+            var compiler = new MalinaCompiler(compilerParameters);
+
+            var context = compiler.Run();
+
+        }
+
+        private static CompilerParameters CreateCompilerParameters()
+        {
+            var compilerParameters = new CompilerParameters();
+            compilerParameters.Pipeline = new CompileToFiles();
+
+            var dir = AssemblyDirectory + '\\' + "Scenarios" + '\\' + GetTestCaseName() + '\\';
+
+            compilerParameters.OutputDirectory = dir + "Recorded" + '\\';
+
+            foreach (var fileName in Directory.EnumerateFiles(dir))
+            {
+                if (fileName.EndsWith(".mlx"))
+                {
+                    compilerParameters.Files.Add(dir + fileName);
+                }
+            } 
+
+            return compilerParameters;
+        }
+
+        public static string AssemblyDirectory
+        {
+            get
+            {
+                return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            }
+        }
+ 
+        private static string GetTestCaseName()
+        {
+            var trace = new StackTrace();
+            return trace.GetFrames().Select(f => f.GetMethod()).Where(m => m.CustomAttributes.Any(a => a.AttributeType.FullName == "NUnit.Framework.TestAttribute")).First().Name;
+        }
+
+        private static bool TestHasAttribute<T>()
+        {
+            var trace = new StackTrace();
+            var method = trace.GetFrames().Select(f => f.GetMethod()).Where(m => m.CustomAttributes.Any(a => a.AttributeType.Equals(typeof(TestAttribute)))).First();
+            return method.CustomAttributes.Any(ca => ca.AttributeType.Equals(typeof(T))) ||
+                method.DeclaringType.CustomAttributes.Any(ca => ca.AttributeType.Equals(typeof(T)));
+        }
+  
+    }
+}

bb8b161a1be46332c2dc052683f1d445c60c296d
Maxim Trushin <trushin@gmail.com>
2016-11-08 11:22:35 -0500

    Added new file to performance test.

--- src/Malina.Parser.PerfomanceTests/PerformanceTests.cs
+++ src/Malina.Parser.PerfomanceTests/PerformanceTests.cs
@@ -29,7 +29,7 @@ namespace Malina.Parser.PerfomanceTests
 
             Console.WriteLine("GetAllTokens Time: {0}", t2 - t1);
             Console.WriteLine("Token Number: {0}", i);
-            Assert.Less(t2 - t1, 6000);
+            Assert.Less(t2 - t1, 7000);
 
             lexer.Reset();
             var parser = new MalinaParser(new CommonTokenStream(lexer));
@@ -44,7 +44,7 @@ namespace Malina.Parser.PerfomanceTests
             t2 = Environment.TickCount;
 
             Console.WriteLine("Parse Time: {0}", t2 - t1);
-            Assert.IsTrue(t2 - t1 < 15000);
+            Assert.Less(t2 - t1,15000);
             Assert.IsFalse(parserErrorListener.HasErrors);
 
 
@@ -52,12 +52,66 @@ namespace Malina.Parser.PerfomanceTests
             parser.Reset();
             parser.AddParseListener(malinaListener);            
             t1 = Environment.TickCount;            
-            //module = parser.module();
+            var module = parser.module();
             t2 = Environment.TickCount;
             Console.WriteLine("DOM Time: {0}", t2 - t1);
-            Assert.IsTrue(t2 - t1 < 20000);
+            Assert.Less(t2 - t1 , 20000);
             Assert.IsFalse(parserErrorListener.HasErrors);
 
         }
+
+        [Test]
+        public void BigFile2()
+        {
+            Console.WriteLine("Starting BigFile");
+            var code = LoadTestCodeRaw();
+            var lexer = new MalinaLexer(new AntlrInputStream(code));
+            //lexer.RemoveErrorListeners();
+            //lexer.ErrorListeners.Clear();
+            var t1 = Environment.TickCount;
+            var i = 0;
+            IToken token = null;
+            for (token = lexer.NextToken(); token.Type != -1; token = lexer.NextToken())
+            {
+                i++;
+            }
+            var t2 = Environment.TickCount;
+
+            Console.WriteLine("GetAllTokens Time: {0}", t2 - t1);
+            Console.WriteLine("Token Number: {0}", i);
+            Assert.Less(t2 - t1, 7000);
+
+            lexer.Reset();
+            var parser = new MalinaParser(new CommonTokenStream(lexer));
+            parser.Interpreter.PredictionMode = PredictionMode.Sll;
+            var malinaListener = new MalinaParserListener();
+            var parserErrorListener = new ErrorListener<IToken>();
+            parser.AddErrorListener(parserErrorListener);
+            //parser.AddParseListener(malinaListener);
+            parser.BuildParseTree = false;
+            t1 = Environment.TickCount;
+            //var module = parser.module();
+            t2 = Environment.TickCount;
+
+            Console.WriteLine("Parse Time: {0}", t2 - t1);
+            Assert.Less(t2 - t1, 15000);
+            Assert.IsFalse(parserErrorListener.HasErrors);
+
+
+            lexer.Reset();
+            parser.Reset();
+            parser.AddParseListener(malinaListener);
+            t1 = Environment.TickCount;
+            var module = parser.module();
+            lexer.Reset();
+            parser.Reset();
+            parser = null;
+            t2 = Environment.TickCount;
+            Console.WriteLine("DOM Time: {0}", t2 - t1);
+            Assert.Less(t2 - t1, 20000);
+            Assert.IsFalse(parserErrorListener.HasErrors);
+
+        }
+
     }
 }

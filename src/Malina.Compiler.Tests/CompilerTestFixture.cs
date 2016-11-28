e020ee84f692d4e9d0144144034faeb71eeac93b
Maxim Trushin <trushin@gmail.com>
2016-11-28 12:14:08 -0500

    Added test AliasWithMissedArgument.

--- src/Malina.Compiler.Tests/CompilerTestFixture.cs
+++ src/Malina.Compiler.Tests/CompilerTestFixture.cs
@@ -126,6 +126,16 @@ namespace Malina.Compiler.Tests
             };
             PerformCompilerTest(errorsExpected);
         }
+        
+        [Test]
+        public void AliasWithMissedArgument()
+        {
+            var errorsExpected = new List<CompilerError>()
+            {
+                new CompilerError(new LexicalInfo("ModuleWithDocument.mlx", 4, 4,-1), "Argument 'name' is missing."),
+            };
+            PerformCompilerTest(errorsExpected);
+        }
 
         [Test]
         public void AliasWithDuplicateArguments()

efe4fa8db52501de08a3e8189a035315bc9f6364
Maxim Trushin <trushin@gmail.com>
2016-12-07 15:53:17 -0500

    Added test AliasInsideSqs.

--- src/Malina.Compiler.Tests/CompilerTestFixture.cs
+++ src/Malina.Compiler.Tests/CompilerTestFixture.cs
@@ -81,6 +81,15 @@ namespace Malina.Compiler.Tests
             PerformCompilerTest(errorsExpected);
         }
 
+
+        
+        [Test, RecordedTest]
+        public void AliasInsideSqs()
+        {
+            PerformCompilerTest();
+        }
+
+
         [Test, RecordedTest]
         public void AliasWithArguments()
         {
